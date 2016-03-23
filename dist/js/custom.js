b688178d49825533acac8d6c8fbb25a816f34064
Rohit <rohitsharma.fboxer@gmail.com>
2016-03-23 17:11:16 +0530

    home page responsive done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -3,10 +3,22 @@
 */
 
 
-$('.bxslider-cli').bxSlider({
- minSlides: 1,
- maxSlides: 4,
- slideWidth: 240,
- slideMargin: 50,
- moveSlides:1,
+
+$(document).ready(function(){
+
+ $('.bxslider-cli').bxSlider({
+  minSlides: 1,
+  maxSlides: 4,
+  slideWidth: 240,
+  slideMargin: 50,
+  moveSlides:1,
+ });
+
+
+
+ $(".btn-toggle").click(function(){
+  $(".navibar").slideToggle();
+ });
+
+
 });
